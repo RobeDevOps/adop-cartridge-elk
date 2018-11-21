0d27b23c704bbbfa6074fbe8537433b825053790
robedevops <rcardenas20@gmail.com>
2018-11-21 16:05:18 -0500

    it is mandatory to have the xml directory

--- jenkins/jobs/dsl/elk_jobs.groovy
+++ jenkins/jobs/dsl/elk_jobs.groovy
@@ -3,17 +3,11 @@ import pluggable.scm.*;
 import adop.cartridge.properties.*;
 
 SCMProvider scmProvider = SCMProviderHandler.getScmProvider("${SCM_PROVIDER_ID}", binding.variables)
-CartridgeProperties cartridgeProperties = new CartridgeProperties("${CARTRIDGE_CUSTOM_PROPERTIES}");
 
 // Folders
 def workspaceFolderName = "${WORKSPACE_NAME}"
 def projectFolderName = "${PROJECT_NAME}"
 def projectScmNamespace = "${SCM_NAMESPACE}"
-
-// Variables
-// **The git repo variables can be changed to the users' git repositories when loading the cartridge and populating the CARTRIDGE_CUSTOM_PROPERTIES with "scm.code.repo.name" and "scm.test.repo.name" properties.
-def skeletonAppgitRepo = cartridgeProperties.getProperty("scm.code.repo.name", "YOUR_APPLICATION_REPO");
-def regressionTestGitRepo = cartridgeProperties.getProperty("scm.test.repo.name", "YOUR_REGRESSION_TEST_REPO");
 
 // ** The logrotator variables should be changed to meet your build archive requirements
 def logRotatorDaysToKeep = 7
