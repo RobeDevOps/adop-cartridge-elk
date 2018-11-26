d54e2f4d019c5dfc2af5f572b60c388c834de35a
robedevops <rcardenas20@gmail.com>
2018-11-26 14:55:44 -0500

    added using printf instead of echo

--- jenkins/jobs/dsl/elk_jobs.groovy
+++ jenkins/jobs/dsl/elk_jobs.groovy
@@ -38,13 +38,11 @@ createIndexPattern.with{
         |do
         |  id=$index_pattern
         |  time_field="@timestamp"
-        |  printf "Creating $index_pattern"
         |  curl -f -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: pattern" "${KIBANA_HOST}/api/saved_objects/index-pattern/$id" -d"{\"attributes\":{\"title\":\"$index_pattern\",\"timeFieldName\":\"$time_field\"}}"
         |  if [ $? -eq 0 ]; then
-        |    printf "\n"
         |    curl -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: pattern" "${KIBANA_HOST}/api/kibana/settings/defaultIndex" -d"{\"value\":\"$id\"}"
         |    if [ $? -eq 0 ]; then
-        |       printf "\n$index_pattern created successfully"
+        |       printf "\\n$index_pattern created successfully"
         |    fi
         |  fi
         |done  
