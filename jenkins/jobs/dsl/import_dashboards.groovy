343fb148433002194a7900a943dd8361145d9a31
robedevops <rcardenas20@gmail.com>
2018-11-26 23:36:41 -0500

    using force to overwrite configuration

--- jenkins/jobs/dsl/import_dashboards.groovy
+++ jenkins/jobs/dsl/import_dashboards.groovy
@@ -20,13 +20,14 @@ importDashboardsJob.with{
     parameters{
         stringParam("KIBANA_HOST", 'http://kibana:5601', "Kibana Server")
         stringParam("DASHBOARD_GIT_REPO", "https://github.com/RobeDevOps/adop-dashboards.git", "Git repository to pull dashboards json files")
+        booleanParam('OVERWRITE', true, 'This flag overwrites any existing configuration. Uncheck the FLAG if this is the first time running the import.')
     }
     steps {
         shell('''#!/bin/bash
         |set +x
         |git clone ${DASHBOARD_GIT_REPO} adop-dashboards
         |cd adop-dashboards
-        |curl -X POST -H "Content-Type: application/json" -H "kbn-xsrf: true" http://kibana:5601/api/kibana/dashboards/import -d @network.json
+        |curl -X POST -H "Content-Type: application/json" -H "kbn-xsrf: true" "${KIBANA_HOST}/api/kibana/dashboards/import?force=${OVERWRITE}" -d @network.json
         |set -x'''.stripMargin()
         )
     }
