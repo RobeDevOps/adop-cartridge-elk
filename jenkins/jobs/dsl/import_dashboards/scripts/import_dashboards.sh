025457fd856ff7b17e69f500cc167b5cf342afbd
robedevops <rcardenas20@gmail.com>
2019-01-11 12:42:55 -0500

    fixed the dasboard name with dashboard

--- jenkins/jobs/dsl/import_dashboards/scripts/import_dashboards.sh
+++ jenkins/jobs/dsl/import_dashboards/scripts/import_dashboards.sh
@@ -9,9 +9,10 @@ fi
 git clone ${DASHBOARD_GIT_REPO} adop-dashboards
 cd adop-dashboards
 
+
 dashboards=(network sonar_metric)
 
-for dashboard in "${dasboards[@]}"
+for dashboard in "${dashboards[@]}"
 do
     curl -X POST -H "Content-Type: application/json" -H "kbn-xsrf: true" "${KIBANA_HOST}/api/kibana/dashboards/import?force=${OVERWRITE}" -d @${dashboard}.json
 done
