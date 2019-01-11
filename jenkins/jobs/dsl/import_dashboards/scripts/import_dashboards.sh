826c22fdfefcad83a82fc7145c0a1dc67958d202
robedevops <rcardenas20@gmail.com>
2019-01-11 12:39:50 -0500

    fixed the dasboard name with dashboard

--- jenkins/jobs/dsl/import_dashboards/scripts/import_dashboards.sh
+++ jenkins/jobs/dsl/import_dashboards/scripts/import_dashboards.sh
@@ -13,6 +13,6 @@ dashboards=(network sonar_metric)
 
 for dashboard in "${dasboards[@]}"
 do
-    curl -X POST -H "Content-Type: application/json" -H "kbn-xsrf: true" "${KIBANA_HOST}/api/kibana/dashboards/import?force=${OVERWRITE}" -d @${dasboard}.json
+    curl -X POST -H "Content-Type: application/json" -H "kbn-xsrf: true" "${KIBANA_HOST}/api/kibana/dashboards/import?force=${OVERWRITE}" -d @${dashboard}.json
 done
 set -x
